cd22b06ea0160338e81791fa56d0d0d73e8e727b
Félix Díez <felixeu31@gmail.com>
2022-07-08 14:22:59 +0200

    Change approach of unit tests

--- Fda.Katas.PasswordValidation/PasswordValidatorTests.cs
+++ Fda.Katas.PasswordValidation/PasswordValidatorTests.cs
@@ -10,35 +10,43 @@ public class Tests
     }
 
     [Test]
-    public void ValidateMinimunCharacters_Invalid_WhenShort()
+    public void Validate_Invalid_WhenNotMinimumCharacters()
     {
         var passwordValidator = new PasswordValidator();
         
         passwordValidator.IsValid("short").Should().BeFalse();
     }
 
-    [Test]
-    public void ValidateMinimunCharacters_Valid_WhenLarge()
-    {
-        var passwordValidator = new PasswordValidator();
-        
-        passwordValidator.IsValid("largepassword").Should().BeTrue();
-    }
+    // [Test]
+    // public void ValidateMinimunCharacters_Valid_WhenLarge()
+    // {
+    //     var passwordValidator = new PasswordValidator();
+    //     
+    //     passwordValidator.IsValid("largepassword").Should().BeTrue();
+    // }
     
 
     [Test]
-    public void ValidateCapital_Invalid_WhenMissing()
+    public void Validate_Invalid_WhenMissingCapitalLetter()
     {
         var passwordValidator = new PasswordValidator();
         
         passwordValidator.IsValid("passwordwithoutcapital").Should().BeFalse();
     }
 
+    // [Test]
+    // public void ValidateCapital_Valid_WhenHasIt()
+    // {
+    //     var passwordValidator = new PasswordValidator();
+    //     
+    //     passwordValidator.IsValid("Passwordwithcapital").Should().BeTrue();
+    // }
+    
     [Test]
-    public void ValidateCapital_Valid_WhenHasIt()
+    public void Validate_Valid_WhenValidPassword()
     {
         var passwordValidator = new PasswordValidator();
         
-        passwordValidator.IsValid("Passwordwithcapital").Should().BeTrue();
+        passwordValidator.IsValid("Passwordwithcapital_1").Should().BeTrue();
     }
 }
